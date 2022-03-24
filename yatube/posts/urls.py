dad48547ae2c45999285b2c9d4db2f7a4892d25a
Александр Дзюба <komrad.dzyuba2014@yandex.ru>
2022-03-24 20:44:06 +0300

    Sp3_V1

---
+++ yatube/posts/urls.py
@@ -0,0 +1,14 @@
+# posts/urls.py
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+
+    #Главная страница
+    path('', views.index, name='glang'),
+    #Список постов после групп
+    path('group/<slug>', views.group_posts, name='group'),
+] 
